57d99d8f937fc1582fa379cfc99610442f8b3353
sheriff <2356874721@qq.com>
2021-01-12 23:07:19 +0800

    init

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+/*
+ * @Author: sheriff
+ * @Description:
+ * @Date: 2021-01-12 22:53:03
+ * @LastEditors: sheriff
+ * @LastEditTime: 2021-01-12 22:53:34
+ * @LastEditDetails:
+ */
+console.log("Hello TypeScript");
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: sheriff
+ * @Description: 
+ * @Date: 2021-01-12 22:53:03
+ * @LastEditors: sheriff
+ * @LastEditTime: 2021-01-12 23:03:10
+ * @LastEditDetails: 
+ */
+console.log("Hello TypeScript")
+
+let a = 1 + 2
+let b = a + 3
+let c = {
+  apple: a,
+  banana: b
+}
+let d = c.apple * 4
